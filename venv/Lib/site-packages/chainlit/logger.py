750f297c9a9e31a5e3e09fedbc30949ac5369e40
23wh1a1204 <23wh1a1204@bvrithyderabad.edu.in>
2026-03-06 18:45:29 +0530

    added tools

--- venv/Lib/site-packages/chainlit/logger.py
+++
@@ -1,8 +0,0 @@
-import logging
-
-logging.getLogger("socketio").setLevel(logging.ERROR)
-logging.getLogger("engineio").setLevel(logging.ERROR)
-logging.getLogger("numexpr").setLevel(logging.ERROR)
-
-
-logger = logging.getLogger("chainlit")
